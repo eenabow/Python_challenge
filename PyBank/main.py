cf5195604a26b3dd19e34deda6e398a6155f4802
eenabow <73721867+eenabow@users.noreply.github.com>
2020-12-01 22:09:44 -0500

    Add files via upload
    Adding more code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,42 @@
+#PyBank homework solution 
+
 import os
 import csv 
 
 Financial_Data = os.path.join("Resources", "PyBank_Resources_budget_data.csv")
-print(type(Financial_Data))
+
 
 Total_Months = 0
-Net_Period = []
-with open(Financial_Data) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = "\t")
-    
-    for row in csv_reader:
-        print("Financial Analysis")
+Total_Net = 0
+Average_Change = []
+previous_profit = int(first_row[1])
+Greatest_profit_increase = []
+Greatest_loss_decrease = []
 
+#Open and read the CSV file
+with open(Financial_Data) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ",")
+    
+    #Skip header row
+    csv_header = next(csv_reader)
+
+    #I don't understand the below 
+    first_row = next(csv_reader)
+    Total_Net += int(first_row[1])
+
+    for row in csv_reader:
+        #calculate total months
+        Total_Months += 1
+        Total_Net += int(row[1])
+
+        #Calculate change in net
+        Net_change = float(row[1]) - previous_profit
+        previous_profit = int(row[1])
+        net_change_list += [row[0]]
+
+        Average_change = sum(net_change_list)/ len(net_change_list)
+        
+print("Total Months:"+ str(Total_Months))
+print("Net over Period:"+ str(Total_Net))
+print("Average Change:"+ str(Average_change))
 
