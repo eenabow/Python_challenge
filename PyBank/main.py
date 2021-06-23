ea8693636fe92909a6e48f5fc3e83c641c2d5c27
Anna Bowers <eenabow@gmail.com>
2021-06-22 22:26:15 -0400

    Proofreading

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,3 @@
-#PyBank homework solution 
 #The total number of months included in the dataset
 #The net total amount of "Profit/Losses" over the entire period
 #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
